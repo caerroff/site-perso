dfe23b3b3c3593ef7a3de030f2214353a52bca7f
Thomas MASIN (SAN DE RGOCC) <56608891+caerroff@users.noreply.github.com>
2023-10-04 17:40:31 +0200

    feat(react) changed title + started home

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Home from './pages/Home/Home'
 import Error from './pages/Error/Error.tsx'
 import './index.css'
 import Header from './components/Header/header.tsx'
@@ -11,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<App />} />
+        <Route path='/' element={<Home />} />
         <Route path='*' element={<Error />} />
       </Routes>
     </Router>
---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <>
+            <div className='flex flex-col bg-indigo-300 m-5 rounded-3xl px-10 py-2 text-center'>
+                <h1 className="text-3xl">Développeur Freelance</h1>
+            </div>
+        </>
+    )
+}
+
+export default Home
