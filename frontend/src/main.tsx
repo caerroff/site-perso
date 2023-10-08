d6ccb668e0cd32c7163533e77e0618147c75814e
Thomas MASIN (SAN DE RGOCC) <56608891+caerroff@users.noreply.github.com>
2023-10-08 17:58:44 +0200

    feat(calendar) added FullCalendar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home'
 import Error from './pages/Error/Error.tsx'
 import Blog from './pages/Blog/Blog.tsx'
 import BlogNew from './pages/Blog/BlogNew.tsx'
+import Calendar from './pages/Calendar/Calendar.tsx'
 import './index.css'
 import Header from './components/Header/header.tsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='/' element={<Home />} />
         <Route path='/blog' element={<Blog />} />
         <Route path='/blog/new' element={<BlogNew />} />
+        <Route path='/calendar' element={<Calendar />} />
         <Route path='*' element={<Error />} />
       </Routes>
     </Router>
---
+++ frontend/src/pages/Calendar/Calendar.tsx
@@ -0,0 +1,15 @@
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+function Calendar() {
+    return (
+        <>
+            <FullCalendar
+                plugins={[dayGridPlugin]}
+                initialView="dayGridMonth"
+            />
+        </>
+    )
+}
+
+export default Calendar
