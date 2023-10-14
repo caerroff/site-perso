71036310b6d54f830cf161dd69c61cd66faf3a26
Thomas MASIN (SAN DE RGOCC) <56608891+caerroff@users.noreply.github.com>
2023-10-14 12:24:38 +0200

    feat(react) styling and added logos

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -34,23 +34,35 @@ function Home() {
                 ) : (
                     <p className="italic text-gray-300 underline">{response.message}</p>
                 )}
-                <div className="text-left p-5 mt-10 h-96 bg-gray-800 rounded-3xl bg-gradient-to-br via-gray-800 from-purple-500">
-                    <h1 className="text-xl font-semibold">Ma Stack</h1>
-                    <h2>☀️ Front-End:</h2>
-                    <ul className="list-disc pl-10">
-                        <li>React.js</li>
-                        <li>Twig</li>
-                    </ul>
-                    <h2>⌨️ Back-End:</h2>
-                    <ul className="list-disc pl-10">
-                        <li>PHP/Symfony</li>
-                        <li>MariaDB / PostgreSQL / MySQL</li>
-                    </ul>
-                    <h2>🛜 Serveur:</h2>
-                    <ul className="list-disc pl-10">
-                        <li>Debian 12</li>
-                        <li>Docker</li>
-                    </ul>
+                <div className="text-left mt-10 sm:h-auto md:h-96 flex flex-col xs:flex-col sm:flex-col md:flex-row">
+                    <div className=" sm:w-full md:w-1/2 p-5 rounded-3xl bg-gray-800 bg-gradient-to-b via-gray-800 from-purple-500">
+                        <h1 className="text-xl font-semibold">Ma Stack</h1>
+                        <h2>☀️ Front-End:</h2>
+                        <ul className="list-disc pl-10">
+                            <li>React.js</li>
+                            <li>Twig</li>
+                        </ul>
+                        <h2>⌨️ Back-End:</h2>
+                        <ul className="list-disc pl-10">
+                            <li>PHP/Symfony</li>
+                            <li>MariaDB / PostgreSQL / MySQL</li>
+                        </ul>
+                        <h2>🛜 Serveur:</h2>
+                        <ul className="list-disc pl-10">
+                            <li>Debian 12</li>
+                            <li>Docker</li>
+                        </ul>
+                    </div>
+                    <div className="sm:w-1/2 grid md:ml-5 xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 h-fit gap-5 sm:mt-12 self-center justify-center items-center">
+                        <img src='/assets/react.svg' alt="react logo" className="h-24 mx-auto xl:col-span-2" />
+                        <img src='/assets/symfony.png' alt="symfony logo" className="h-24 pt-0 mx-auto xl:col-span-2" />
+                        <div className='h-fit inline-block align-middle w-fit mx-auto xl:col-span-4'>
+                            <img src='/assets/htmlcss.png' alt="html logo" className='h-14 mr-1 mx-auto' />
+                            <img src='/assets/js.png' alt="js logo" className='h-12 mt-2 mx-auto' />
+                        </div>
+                        <img src='/assets/docker.webp' alt="docker logo" className="h-24 mx-auto xl:col-span-2" />
+                        <img src='/assets/mariadb.png' alt="mariadb logo" className="h-24 mx-auto col-span-2" />
+                    </div>
                 </div>
                 <div className="mt-26 mb-48 p-5 text-right">
                     <h1 className="underline text-2xl font-semibold">Right part</h1>
