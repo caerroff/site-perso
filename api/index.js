748b89f14e0720bcb8e419b5242282c4300cfd84
Thomas MASIN (SAN DE RGOCC) <56608891+caerroff@users.noreply.github.com>
2023-10-15 18:08:10 +0200

    feat(backend) switched to nodejs and express

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const api = require('express')();
+const PORT = 3000;
+api.listen(
+    PORT,
+    () => console.log(`API running on port http://localhost:${PORT}`)
+)
+
+api.get('/', (req, res) => {
+    res.status(200).send({ message: 'Développeur React JS et Symfony' });
+})
