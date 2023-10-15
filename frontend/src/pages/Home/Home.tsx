8e0948c2dd551baadf2e927cb14eefc8bf4949e8
Thomas MASIN (SAN DE RGOCC) <56608891+caerroff@users.noreply.github.com>
2023-10-15 17:23:25 +0200

    feat(front) API

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -15,7 +15,7 @@ function Home() {
         setLoading(true)
         async function fetchApi() {
             try {
-                const response = await fetch('http://127.0.0.1:3000')
+                const response = await fetch('http://0.0.0.0:3000')
                 const data = await response.json()
                 setResponse(data)
             } catch (error) {
