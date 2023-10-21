cc55fd36b4d615857e18407817f47a9069673444
Thomas MASIN (SAN DE RGOCC) <56608891+caerroff@users.noreply.github.com>
2023-10-21 16:58:19 +0200

    feat(admin) changed layout

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -29,7 +29,12 @@ function Login() {
                 <h1 className='text-xl md:text-3xl'>Login</h1>
                 <div className='flex flex-col items-center space-y-5'>
                     <input className='bg-gray-500 h-10 w-72 text-white rounded-xl text-center focus:outline focus:outline-slate-400' placeholder='Login' id="login" />
-                    <div className='rounded-2xl'><input type='password' className='focus:outline focus:outline-slate-400 bg-gray-500 h-10 w-64 text-white rounded-l-xl text-center outline-' placeholder='Password' id='password' ></input><button id='eye' className='fa fa-eye h-10 bg-gray-500 text-black p-1 rounded-r-xl' onClick={() => { showPassword() }} /></div>
+
+                    <div className='rounded-2xl space-x-2'>
+                        <input type='password' className='focus:outline focus:outline-slate-400 bg-gray-500 h-10 w-64 text-white rounded-xl text-center' placeholder='Password' id='password' ></input>
+                        <button id='eye' className='fa fa-eye h-10 bg-gray-500 text-black p-1 rounded-xl focus:outline focus:outline-slate-400' onClick={() => { showPassword() }} />
+                    </div>
+
                     <button className='bg-blue-500 hover:bg-blue-800 active:bg-blue-900 text-white border border-blue-500 hover:border-red-300 font-bold p-2 px-5 rounded-2xl'
                         onClick={() => login()}>Login</button>
                 </div>
